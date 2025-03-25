22f1f7d17fc195e2013babad6b0dca0cb5402381
RicardoMunoz <ricardomunoz4250@gmail.com>
2025-03-24 18:14:22 -0700

    Initial commit

---
+++ src/main/java/triviaGame/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package triviaGame;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author ricardomunoz
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+    }
+}
