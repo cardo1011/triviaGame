1290fba59d2243da5ebd274dbfdb62d01c3281fc
RicardoMunoz <ricardomunoz4250@gmail.com>
2025-04-02 13:28:39 -0700

    added the labels to each answer choice and updated the correct answer with the prepended lable

--- src/main/java/triviaGame/Main.java
+++ src/main/java/triviaGame/Main.java
@@ -6,6 +6,8 @@ package triviaGame;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
 
 
 /**
@@ -16,48 +18,98 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-
-
-//        //non-dynamic URL of the api used to get trivia questions from
-//        String API_URL = "https://opentdb.com/api.php?amount=2&category=9&type=multiple";
-//
-//        // Building a "GET" request.
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .GET() //This line is technically unnecessary because the default HttpRequest is a GET request but leaving for readability of code purposes.
-//                .uri(URI.create(API_URL))
-//                .build();
-//
-//        //**HttpClient objects are created to be able to send requests and receive responses
-//        //Here the client object is being created
-//        HttpClient client = HttpClient.newBuilder().build();
-//
-//        //We can now create a response using the response we get after sending the client object with the GET request to the API
-//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//
-//        //Printing out the response body to confirm we are receiving the JSON information in the form of a raw string
-//        String jsonData = response.body();
-//        System.out.println(jsonData);
-//
-//
-//        //Create a Gson instance to parse the raw JSON response into an ApiResponse object.
-//        GsonBuilder gsonBuilder = new GsonBuilder();
-//        Gson gson = gsonBuilder.create();
-//        ApiResponse resObject = gson.fromJson(jsonData, ApiResponse.class);
-
-
-
-
-
-        //Testing the output of the response using dot notation with the ApiResponse class and the Question class created to handle the results array of objects
-//        int responseCode = resObject.getResponse_code();
-//        System.out.println(responseCode);
-//
-//        Question[] results = resObject.getResults();
-//        System.out.println(Arrays.toString(results));
-
+        //Fetching questions from API
         ApiResponse apiResponse = ApiResponse.parseResponse();
-        System.out.println("RESULTS: " + Arrays.toString(apiResponse.getResults()));
+//        System.out.println("RESULTS: " + Arrays.toString(apiResponse.getResults())); //this verifies the amount of questions and category is returning as intended
 
+        //Creating an array of questions by doing a shallow copy of the API response
+        Question[] questions = apiResponse.getResults().clone();
+
+
+        //Rules and instructions of the game
+        System.out.println("*********************************************************************************************************************************************************************************");
+        System.out.print("Welcome to the trivia game! \n\n" +
+                "- You will be prompted with 1 question at a time and will answer 7 questions total. \n" +
+//                "- There will be a 70 second timer going on while you play.\n" +
+                "- Answer the questions as fast as you can by typing in the letter corresponding to your answer and then pressing ENTER to submit your answer.\n" +
+                "- At the end of the game, you will be shown your score and the right answer to each question.\n" +
+                "\n *** Press ENTER when you are ready to play! ***");
+
+        Scanner input = new Scanner(System.in);
+//        System.out.println(Arrays.toString(questions));
+        System.out.println();
+
+
+        if(input.nextLine().isEmpty()){     // Will check if nextLine isEmpty in order to start the game when the user presses ENTER
+
+            int i = 0;
+            int score = 0;
+
+            while( i < questions.length){
+
+
+                String[] arrayOfIncorrectAnswersFromAPI = questions[i].getIncorrect_answers();
+                String correctAnswer = questions[i].getCorrect_answer();
+
+                String activeQuestion = questions[i].getQuestion();
+
+                String[] arrayOfshuffledAnswers = questions[i].shuffleAnswers(correctAnswer, arrayOfIncorrectAnswersFromAPI);
+
+                String[] arrayOflabeledAndShuffledAnswers = questions[i].addLabelsToShuffledAnswers(arrayOfshuffledAnswers);
+
+
+                System.out.println(activeQuestion); //prints out the active question from the questions array
+
+                //prints out the answer choices
+                for (String answer : arrayOflabeledAndShuffledAnswers) {
+                    System.out.println(answer);
+                }
+
+//                System.out.println(correctAnswer);
+
+
+                String userAnswer = input.nextLine();
+
+                switch(userAnswer) {
+                    case "A", "a":
+                        userAnswer = "A";
+                        break;
+                    case "B", "b":
+                        userAnswer = "B";
+                        break;
+                    case "C", "c":
+                        userAnswer = "C";
+                        break;
+                    case "D", "d":
+                        userAnswer = "D";
+                        break;
+                    default:
+                        userAnswer = "X";
+                }
+
+                char alphanumericalRepresentationOfTheCorrectAnswer= questions[i].getCorrect_answer().charAt(1);
+
+                if(userAnswer.equals(String.valueOf(alphanumericalRepresentationOfTheCorrectAnswer))){
+                    score++;
+                }
+
+                if(userAnswer.equals(String.valueOf(correctAnswer))){
+                    score++;
+                }
+
+                System.out.println();
+                i++;
+
+                }
+
+            System.out.println("You got " + score + " out of " + questions.length + " question(s) correctly answered.");
+
+            System.out.println();
+            System.out.println("The questions INcorrectly  answered: ");
+
+
+            }
+
+        }
 
     }
-}
--- src/main/java/triviaGame/Question.java
+++ src/main/java/triviaGame/Question.java
@@ -4,6 +4,13 @@
  */
 package triviaGame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author ricardomunoz
@@ -16,6 +23,7 @@ public class Question {
         private String question;
         private String correct_answer;
         private String[] incorrect_answers;
+        private String[] shuffledAnswers;
 
         public Question(String type,
                         String difficulty,
@@ -31,6 +39,14 @@ public class Question {
             this.incorrect_answers =  incorrect_answers.clone();
 
             }
+
+    public String[] getShuffledAnswers() {
+        return shuffledAnswers;
+    }
+
+    public void setShuffledAnswers(String[] shuffledAnswers) {
+        this.shuffledAnswers = shuffledAnswers;
+    }
 
     public String getType() {
         return type;
@@ -68,8 +84,8 @@ public class Question {
         return correct_answer;
     }
 
-    public void setCorrect_answer(String correct_answer) {
-        this.correct_answer = correct_answer;
+    public void setCorrect_answer(String answer) {
+        this.correct_answer = answer;
     }
 
     public String[] getIncorrect_answers() {
@@ -80,10 +96,61 @@ public class Question {
         this.incorrect_answers = incorrect_answers;
     }
 
+    public String[] shuffleAnswers(String correctAnswer, String[] arrayOfIncorrectAnswers){
+        //Creating a list array to merge the correct answer and the incorrect answers together
+        List<String> answers = new ArrayList<>(Arrays.asList(arrayOfIncorrectAnswers));
+
+        //Appending the correct answer to the incorrect answers
+        answers.add(correctAnswer);
+
+        //Shuffling the order of the answers so the correct answer is not always at the same index
+        Collections.shuffle(answers);
+
+        //updating shuffledAnswers array in order to update the correct answer
+        setShuffledAnswers(answers.toArray(new String[0]));
+
+        return answers.toArray(new String[0]);
+    }
+
+    public String[] addLabelsToShuffledAnswers(String[] arrayOfShuffledAnswers) {
+
+        //Assigning the traditional A,B,C,D answer choices to each answer to be able to answer quicker while in game
+        char alphabeticalAnswerChoice = 'A';
+        for(int i = 0; i < arrayOfShuffledAnswers.length; i++){
+            arrayOfShuffledAnswers[i] = "(" +  alphabeticalAnswerChoice + ") " + arrayOfShuffledAnswers[i];
+            alphabeticalAnswerChoice++; //incrementing a char actually increments the unicode and therefore allows us to loop simultaneously through the alphabet
+        }
+
+        //updating shuffledAnswers with the new array that prepended labels to each element in the array
+        setShuffledAnswers(arrayOfShuffledAnswers);
+
+
+        //***The correct_answer needs to be updated to the correct answer with the prepended label.
+
+        //Creating a regex that will match the correct answer with its corresponding label
+        String regex = "\\(" + "[A-Z]" + "\\)" + "\\s" + correct_answer;
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE); //making sure the regex match is case insensitive
+
+        //iterate through the shuffledAnswers array until we find a match for the regex and then update the correct_answer with the corresponding prepended label
+        for (String shuffledAnswer : shuffledAnswers) {
+
+            Matcher matcher = pattern.matcher(shuffledAnswer); //using .matcher() because we want an exact match of the correct answer
+            boolean found = matcher.matches();
+
+            if (found) {
+//                System.out.println("********" + correct_answer + "*********");
+                setCorrect_answer(shuffledAnswer);
+//                System.out.println("Updated correct answer: " + correct_answer + "*********");
+            }
+        }
+        return arrayOfShuffledAnswers;
+    }
+
+
     @Override
     public String toString(){
 
-        return String.format("Type: %s \n" +
+        return String.format("\nType: %s \n" +
                         "Difficulty: %s \n" +
                         "Category: %s \n" +
                         "Question: %s \n" +
